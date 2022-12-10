4acd520ee105888747bcab6d880e3bcbe6f2efad
Rodrigo Tavares <rodrigost23@users.noreply.github.com>
2022-12-10 14:00:40 -0400

    Fix reshack download and finding

--- install.ps1
+++ install.ps1
@@ -14,6 +14,33 @@ Param(
     $7zPath
 )
 
+# Source: http://blogs.msdn.com/b/jasonn/archive/2013/06/11/8594493.aspx
+function DownloadFile($url, $targetFile)
+{
+   $uri = New-Object "System.Uri" "$url"
+   $request = [System.Net.HttpWebRequest]::Create($uri)
+   $request.set_Timeout(15000) #15 second timeout
+   $response = $request.GetResponse()
+   $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+   $responseStream = $response.GetResponseStream()
+   $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+   $buffer = new-object byte[] 10KB
+   $count = $responseStream.Read($buffer,0,$buffer.length)
+   $downloadedBytes = $count
+   while ($count -gt 0)
+   {
+       $targetStream.Write($buffer, 0, $count)
+       $count = $responseStream.Read($buffer,0,$buffer.length)
+       $downloadedBytes = $downloadedBytes + $count
+       Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+   }
+   Write-Progress -activity "Finished downloading file '$($url.split('/') | Select -Last 1)'"
+   $targetStream.Flush()
+   $targetStream.Close()
+   $targetStream.Dispose()
+   $responseStream.Dispose()
+}
+
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $dllPath = Join-Path $7zPath "7z.dll"
 
@@ -35,31 +62,33 @@ Function Test-CommandExists {
 
 } 
 
-$env:Path += ";$scriptPath\bin"
+$localBinDir = Join-Path "$scriptPath" "bin"
+$env:Path += ";$localBinDir"
 $executable = "ResourceHacker.exe"
 # Downloads Resource Hacker if not found
 if (-not (Test-CommandExists "$executable")) {
     $zipFilePath = Join-Path "$env:TEMP" "reshack.zip"
 
-    Write-Output "Downloading Resource Hacker..."
-    
-    Remove-Item -Path $zipFilePath
+    if (Test-Path $zipFilePath) {
+        Remove-Item -Path $zipFilePath -ErrorAction Stop
+    }
 
-    (New-Object Net.WebClient).DownloadFile("http://www.angusj.com/resourcehacker/resource_hacker.zip", $zipFilePath)
-
-    $destinationPath = "$env:TEMP"
+    DownloadFile "http://www.angusj.com/resourcehacker/resource_hacker.zip" $zipFilePath
 
     $zipfile = (New-Object -Com Shell.Application).NameSpace($zipFilePath)
-    $destination = (New-Object -Com Shell.Application).NameSpace($destinationPath)
 
+    $destinationPath = Join-Path -Path $env:TEMP -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension($zipFilePath))
+    New-Item -Path $destinationPath -ItemType Directory -ErrorAction SilentlyContinue
+    $destination = (New-Object -Com Shell.Application).NameSpace($destinationPath)
     $destination.CopyHere($zipfile.Items(), 0x14)
 
-    $extractedPath = Join-Path -Path $destinationPath -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension($zipFilePath))
+    Write-Output "Copying to $localBinDir..."
+    New-Item -Path $localBinDir -ItemType Directory -ErrorAction SilentlyContinue
 
-    New-Item -Path (Join-Path "$scriptPath" "bin") -ItemType Directory -ErrorAction SilentlyContinue
-
-    Copy-Item -Path (Join-Path "$extractedPath" "$executable") -Destination (Join-Path $scriptPath 'bin' $executable)
+    Copy-Item -Path (Join-Path "$destinationPath" "$executable") -Destination (Join-Path $localBinDir $executable)
 }
+
+Write-Output "Editing 7-Zip resources..."
 
 ResourceHacker.exe -open (Join-Path $env:SystemRoot "system32" "zipfldr.dll") -save "icons\zip.ico" -action extract -log NUL -mask "ICONGROUP,101"
 ResourceHacker.exe -open (Join-Path $env:SystemRoot "SystemResources" "zipfldr.dll.mun") -save "icons\zip.ico" -action extract -log NUL -mask "ICONGROUP,101"
@@ -95,3 +124,5 @@ ResourceHacker.exe -open $dllPath -save $dllPath -action addoverwrite -resource 
 ResourceHacker.exe -open $dllPath -save $dllPath -action addoverwrite -resource icons\img.ico -log NUL -mask "ICONGROUP,22,0"
 ResourceHacker.exe -open $dllPath -save $dllPath -action addoverwrite -resource icons\zip.ico -log NUL -mask "ICONGROUP,23,0"
 ResourceHacker.exe -open $dllPath -save $dllPath -action addoverwrite -resource icons\img.ico -log NUL -mask "ICONGROUP,24,0"
+
+Write-Output "Finished."
