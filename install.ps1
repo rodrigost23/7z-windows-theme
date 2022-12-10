ebec0fb14542459ad160b017bad84c15d2ccef61
Rodrigo Tavares <rodrigost23@users.noreply.github.com>
2022-12-10 14:53:18 -0400

    Fix progress

--- install.ps1
+++ install.ps1
@@ -107,7 +107,7 @@ Copy-Item -Path $dllPath -Destination $backupDllPath
 Write-Output "Editing 7-Zip DLL..."
 
 # Number of icons to show progress:
-$totalIcons = 24
+$totalIcons = 25
 $iconMap = @{
     "zip.ico" = @(0,1,2,3,4,5,6,9,10,11,12,13,14,16,19,23);
     "cab.ico" = @(7);
@@ -128,8 +128,8 @@ foreach ($iconPair in $iconMap.GetEnumerator()) {
             "-log", "NUL",
             "-mask", "ICONGROUP,$iconNumber,"
         )
+        $i++
     }
-    $i++
 }
 
 Write-Output "Refreshing cache..."
